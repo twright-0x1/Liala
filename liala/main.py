1ae580f7ef43657546cbbfc01e417e768e24368d
twright-0x1 <twright.0x1@icloud.com>
2023-12-28 15:52:28 +0000

    Rearranged some of the test code

--- liala/main.py
+++ liala/main.py
@@ -1,5 +1,20 @@
 from utils.helper_functions import *
 from utils.sentence_parser import SentenceParser
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Define the menu items
+menu_items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the selected item from the form
+        selected_item = request.form['menu']
+        return render_template('index.html', menu_items=menu_items, selected_item=selected_item)
+    else:
+        return render_template('index.html', menu_items=menu_items, selected_item=None)
 
 # create a SentenceParser object
 parser = SentenceParser()
@@ -11,6 +26,9 @@ sentence = "The quick, brown fox jumps over the lazy dog."
 result = parser.parse_sentence(sentence)
 print(result)
 
-choices = ["Option 1", "Option 2", "Option 3"]
-choice = get_menu_choice(choices)
-print(f"You chose option {choice}")
+# choices = ["Option 1", "Option 2", "Option 3"]
+# choice = get_menu_choice(choices)
+# print(f"You chose option {choice}")
+
+if __name__ == '__main__':
+    app.run(debug=True)
